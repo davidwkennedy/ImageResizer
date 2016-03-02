49b215d2b5684338c1f71c9732522161cc76d5e4
David Kennedy <davidwkennedy@gmail.com>
2016-03-02 11:44:20 -0800

    Adding first code - version 0.0.0.1 if you will

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ImageResizer
 {
@@ -10,6 +8,38 @@ namespace ImageResizer
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("About to rezise all jpeg (or jpg) images in the current directory. These NEW files will all be saved to a new sub-directory named 'Resized Image Output'. ");
+                Console.Write("EXISTING FILES WILL BE LEFT UNTOUCHED. After running, please review the auto-generated log.txt file. ");
+                Console.Write("Smash the 'Any' key to continue. ");
+                Console.ReadKey();
+
+                var files = Directory.GetFiles("./", "*.jpeg").Union(Directory.GetFiles("./", "*.jpg")).ToArray();
+                const string subPath = "Resized Image Output";
+                var exists = Directory.Exists(subPath);
+                if (!exists) Directory.CreateDirectory(subPath);
+
+                foreach (var file in files)
+                {
+                    var message = DateTime.Now + "\tResizing : " + file;
+                    Console.WriteLine(message);
+                    File.AppendAllText("./log.txt", message + Environment.NewLine);
+                    var resizedImage = Jpegger.ResizeOriginal(File.ReadAllBytes(file));
+                    Console.Write(resizedImage.Length);
+                    File.WriteAllBytes("./" + subPath + "/" + file, resizedImage);
+                }
+                File.AppendAllText("./log.txt", "Success!" + Environment.NewLine);
+                Console.WriteLine("Success! Mash the 'Any' key to exit.");
+
+            }
+            catch (Exception e)
+            {
+                File.AppendAllText("./log.txt", DateTime.Now + " ERROR: " + e.Message + Environment.NewLine);
+            }
+
         }
+
+
     }
 }
